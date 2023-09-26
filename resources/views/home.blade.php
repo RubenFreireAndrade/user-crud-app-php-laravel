185ffb6b78a157648ef91e896cc93d4a21bbf19a
RubenFreireAndrade <rubenswork20@gmail.com>
2023-09-27 00:26:49 +0100

    Pushing full project first time.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    include("header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to our website</h1>
+    <a href="login.php">LOGIN</a>
+</body>
+</html>
+
+<?php
+    include("footer.html");
+?>
